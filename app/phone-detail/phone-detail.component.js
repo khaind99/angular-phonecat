faaea583fd4e725dd6e01539484840f871050d73
Nguyen Dinh Khai <khaind.work@gmail.com>
2019-09-05 14:41:23 +0700

    add modal-bootstrap

--- app/phone-detail/phone-detail.component.js
+++ app/phone-detail/phone-detail.component.js
@@ -2,19 +2,24 @@
 
 // Register `phoneDetail` component, along with its associated controller and template
 angular.
-  module('phoneDetail').
-  component('phoneDetail', {
-    templateUrl: 'phone-detail/phone-detail.template.html',
-    controller: ['$routeParams', 'Phone',
-      function PhoneDetailController($routeParams, Phone) {
-        var self = this;
-        self.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
-          self.setImage(phone.images[0]);
-        });
+module('phoneDetail').
+component('phoneDetail', {
+  templateUrl: 'phone-detail/phone-detail.template.html',
+  controller: ['$routeParams', 'Phone',
+    function PhoneDetailController($routeParams, Phone) {
+      var self = this;
+      $('#myModal').on('shown.bs.modal', function () {
+        $('#myInput').focus()
+      })
+      self.phone = Phone.get({
+        phoneId: $routeParams.phoneId
+      }, function (phone) {
+        self.setImage(phone.images[0]);
+      });
 
-        self.setImage = function setImage(imageUrl) {
-          self.mainImageUrl = imageUrl;
-        };
-      }
-    ]
-  });
+      self.setImage = function setImage(imageUrl) {
+        self.mainImageUrl = imageUrl;
+      };
+    }
+  ]
+});
--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -2,13 +2,13 @@
 
 // Register `phoneList` component, along with its associated controller and template
 angular.
-  module('phoneList').
-  component('phoneList', {
-    templateUrl: 'phone-list/phone-list.template.html',
-    controller: ['Phone',
-      function PhoneListController(Phone) {
-        this.phones = Phone.query();
-        this.orderProp = 'age';
-      }
-    ]
-  });
+module('phoneList').
+component('phoneList', {
+  templateUrl: 'phone-list/phone-list.template.html',
+  controller: ['Phone',
+    function PhoneListController(Phone) {
+      this.phones = Phone.query();
+      this.orderProp = 'age';
+    }
+  ]
+});
